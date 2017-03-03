55c7412e8800823183f283965e7e9bb36b27cde4
Yalım Doğan <yalimdre@gmail.com>
2017-03-03 16:29:48 +0300

    Projection of Agents

---
+++ Assets/ProjectionTest/ProjectedAgent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+ * Written by Yalım Doğan
+ * 
+ * A very basic agent mode that has a stickman prefab and maintains a velocity starting from an initial spawn point
+ * 
+ */
+
+
+
+
+public class ProjectedAgent: MonoBehaviour {
+
+    // Properties
+    Vector3 velocity;
+    Vector3 pos;
+    int AgentId;
+
+    ProjectedAgent(Vector3 initialPos, Vector3 initialVelocity, int id)
+    { 
+        
+    }
+
+}
